c5c86fd9ccf9a10bcc462bed6ffca3bd366f25cb
Wim Coenen <wim@akimbo.be>
2023-12-08 23:26:42 +0100

    Highlight prices at or below treshold.

---
+++ WalleeCharging.WebApp/wwwroot/js/price-chart.js
@@ -0,0 +1,55 @@
+const ctx = document.getElementById('priceChart');
+
+const chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: 'Prices',
+      data: prices
+    }]
+  },
+  options: {
+      events: ['click'],
+      scales: {
+          y: {
+          beginAtZero: true
+          }
+      }
+  }
+});
+
+function clickHandler(click)
+{
+  const points = chart.getElementsAtEventForMode(click, 'nearest', {intersect: true}, true);
+  const clickedPrice = prices[points[0].index];
+
+  // set max price textbox
+  document.getElementById('maxPriceEurocentPerMWh').setAttribute('value', clickedPrice);
+
+  // highlight bars representing a value equal to or below the selected one
+  highlightBarsWithPriceEqualOrBelow(clickedPrice);
+}
+chart.canvas.onclick = clickHandler;
+
+function highlightBarsWithPriceEqualOrBelow(selectedPrice) {
+    // this clears off any tooltip highlights
+    chart.update();
+    chart.activeElements = [];
+
+    var backgroundColors = [];
+    for (var i=0; i<prices.length; i++) {
+        if (prices[i] <= selectedPrice)
+        {
+            backgroundColors.push('#9ad0f5');
+        }
+        else
+        {
+            backgroundColors.push('lightgrey');
+        }
+    }
+  
+    var dataset = chart.data.datasets[0];
+    dataset.backgroundColor = backgroundColors;
+    chart.update();
+  }
