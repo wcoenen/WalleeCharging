90b346e3d31f03df96d9f661662ff4bc144e905c
Wim Coenen <wim@akimbo.be>
2023-12-08 17:08:13 +0100

    Remove windows-specific NoSleep.cs

--- WalleeCharging.WebApp/Services/SignalRNotificationSink.cs
+++ WalleeCharging.WebApp/Services/SignalRNotificationSink.cs
@@ -22,8 +22,6 @@ public class SignalRNotificationSink : INotificationSink
         float currentLimitAmpere,
         string message)
     {
-        NoSleep.PreventSleep();
-
         // see also WalleeCharging.WebApp/wwwroot/js/signalRSubscriber.js
         return _hubContext.Clients.All.SendAsync(
                     "ReceiveControlLoopNotification",
