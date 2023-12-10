c851a4f4bcbf8c74a4c9895a939f72d0347c0a3a
Wim Coenen <wim@akimbo.be>
2023-12-11 00:04:34 +0100

    Tweak deploy script to not copy appsettings*.json, don't create publish subfolder on target.

--- WalleeCharging.WebApp/deploy.ps1
+++ WalleeCharging.WebApp/deploy.ps1
@@ -1,3 +1,16 @@
 $ErrorActionPreference="Stop"
+$publishFolder=(Join-Path $PSScriptRoot '\bin\Release\net8.0\publish')
+
+# dotnet publish seems to keep old files around, so let's clean them first
+if (Test-Path $publishFolder)
+{
+    write-host "Cleaning $publishFolder ..."
+    Remove-Item -Force -Recurse $publishFolder
+}
+
 dotnet publish
-& "C:\Program Files\PuTTY\pscp.exe" -r "$PSScriptRoot\bin\Release\net8.0\publish\" wim@wallee.local:/home/wim/WalleeCharging/
+
+# dotnet publish seems to include development-only files, so let's delete those
+Remove-Item (Join-Path $publishFolder appsettings*.json)
+
+scp -r "$publishFolder\*" wim@wallee.local:/home/wim/WalleeCharging
