bfd6186c121208b9c238181bdbe538f2d4638952
Wim Coenen <wim@akimbo.be>
2025-10-10 09:27:54 +0200

    offer to remember deployment target

--- WalleeCharging.WebApp/deploy.ps1
+++ WalleeCharging.WebApp/deploy.ps1
@@ -1,11 +1,30 @@
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$targetHostname,
-    [Parameter(Mandatory=$true)]
-    [string]$targetPath
-)
-
+# Deploy WalleeCharging.WebApp to a remote Linux system via SSH/SCP.
 $ErrorActionPreference="Stop"
+
+$deployConfig = "$PSScriptRoot\deploy.json"
+if (Test-Path $deployConfig)
+{
+    $config = Get-Content $deployConfig | ConvertFrom-Json
+    $targetHostname = $config.targetHostname
+    $targetPath = $config.targetPath
+}
+else {
+    # prompt for target if no config file is found
+    $targetHostname = (Read-Host 'Enter user@hostname target')
+    $targetPath = (Read-Host 'Enter target folder')
+
+    # offer to save config for next time
+    $saveConfig = (Read-Host 'Save target info to deploy.json for next time? (y/n)')
+    if ($saveConfig -eq 'y')
+    {
+        $config = [PSCustomObject]@{
+            targetHostname = $targetHostname
+            targetPath = $targetPath
+        }
+        $config | ConvertTo-Json | Set-Content $deployConfig
+        Write-Host "Saved config to $deployConfig"
+    }
+}
 
 $publishFolder=(Join-Path $PSScriptRoot '\bin\Release\net8.0\publish')
 
@@ -18,7 +37,7 @@ if (Test-Path $publishFolder)
 
 dotnet publish
 
-# dotnet publish seems to include development-only files, so let's delete those
+# Deployment should not change the configuration on the target system
 Remove-Item (Join-Path $publishFolder appsettings*.json)
 
 # Stop WalleeCharging on deployment target, copy files, and restart
